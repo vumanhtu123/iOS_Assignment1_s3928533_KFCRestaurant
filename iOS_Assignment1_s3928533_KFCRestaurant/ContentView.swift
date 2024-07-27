93d34e7906a642c665d9b6431fa4c891a909ffe4
vumanhtu <vutu1706@gmail.com>
2024-07-27 21:11:09 +0700

    first commit

---
+++ iOS_Assignment1_s3928533_KFCRestaurant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_Assignment1_s3928533_KFCRestaurant
+//
+//  Created by MacBook Pro Của A Tú on 27/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_Assignment1_s3928533_KFCRestaurant/iOS_Assignment1_s3928533_KFCRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Assignment1_s3928533_KFCRestaurantApp.swift
+//  iOS_Assignment1_s3928533_KFCRestaurant
+//
+//  Created by MacBook Pro Của A Tú on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Assignment1_s3928533_KFCRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
